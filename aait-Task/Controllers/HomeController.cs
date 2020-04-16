bed8bbe7e53bfeb51ee0d3c732f889a258741d50
dewydar <57104893+dewydar@users.noreply.github.com>
2020-04-17 01:06:25 +0200

    first commit

---
+++ aait-Task/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using aait_Task.Models;
+using System.Web.Http.Cors;
+using System.ServiceModel.Activities;
+
+namespace aait_Task.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+            
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        //public async Task<ActionResult> getAll()
+        //{
+        //    s.WebService sss = new s.WebService();
+        //    string urlAction = String.Format("/aait/Service/WebService.asmx/Request_SEL");
+        //    var T = await GetWSObject(urlAction);
+        //    return Json(T, JsonRequestBehavior.AllowGet);
+
+        //}
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        //public async Task <List<TestClass>> GetWSObject(string uriActionString)
+        //{
+        //    List<TestClass> list;
+        //    try
+        //    {
+        //        using (var client = new HttpClient())
+        //        {
+        //            client.BaseAddress = new Uri(@"http://dev.aait.com.sa");
+        //            client.DefaultRequestHeaders.Accept.Clear();
+        //            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        //            HttpResponseMessage response = await client.GetAsync(uriActionString);
+        //            response.EnsureSuccessStatusCode();
+        //            var r = response.Content.ReadAsStringAsync().Result;
+        //             list= JsonConvert.DeserializeObject<List<TestClass>>(response.Content.ReadAsStringAsync().Result);
+        //            //var ssss= JsonConvert.DeserializeObject<T>(((HttpResponseMessage)response).Content.ReadAsStringAsync().Result);
+        //            //returnValue = JsonConvert.DeserializeObject<T>(((HttpResponseMessage)response).Content.ReadAsStringAsync().Result);
+        //        }
+        //        return list;
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        throw (e);
+        //    }
+        //}
+
+    
+    }
+}
---
+++ aait-Task/Models/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aait_Task.Models
+{
+    public class TestClass
+    {
+        public double DOMAIN_ID { get; set; }
+        public string V_DOMAIN_NAME { get; set; }
+    }
+}
